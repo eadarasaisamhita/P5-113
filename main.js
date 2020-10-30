41cd4c0ab746739a31fd3a03152b83e965e9636f
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-10-29 21:43:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() 
+{
+
+}
+
+function setup() 
+{
+ canvas=createCanvas(640,400);
+ canvas.position(150,150);
+ video=createCapture(VIDEO);
+ video.hide();
+}
+
+function draw() 
+{
+  image(video,0,0,640,480);
+}
+
+function take_snapshot() 
+{
+  save('student_name.png');
+}
